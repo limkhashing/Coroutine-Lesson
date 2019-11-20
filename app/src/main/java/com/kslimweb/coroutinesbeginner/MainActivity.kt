4083c082bd2e95b6e11e36ef2ada4b609ca428b5
Lim Kha Shing <kslim5703@gmail.com>
2019-11-20 22:08:12 +0800

    more example with deferred async await timeout

--- app/src/main/java/com/kslimweb/coroutinesbeginner/MainActivity.kt
+++ app/src/main/java/com/kslimweb/coroutinesbeginner/MainActivity.kt
@@ -6,6 +6,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlin.system.measureTimeMillis
+
+const val JOB_TIMEOUT = 1900L
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,9 +27,141 @@ class MainActivity : AppCompatActivity() {
             // Default is for doing any heavy computation work
             // IO, Main, Default
             CoroutineScope(IO).launch {
-                fakeApiRequest()
+//                fakeApiRequest()
 //                fakeApiRequestTimeout()
+//                fakeApiRequestTimeoutSimple()
+//                fakeApiJobParallelRequest()
+//                fakeApiJobJoinRequestCaller()
+                fakeApiAsyncAwaitDeferredRequest()
             }
+
+        }
+    }
+
+    private suspend fun fakeApiRequestTimeoutSimple() {
+        coroutineScope {
+            val job = launch {
+
+                val result1 = getResult1FromApi() // wait until job is done
+                if (result1 == "Result #1") {
+                    setTextOnMainThread("Got $result1")
+
+                    val result2 = getResult2FromApi() // wait until job is done
+                    if (result2 == "Result #2") {
+                        setTextOnMainThread("Got $result2")
+                    } else {
+                        setTextOnMainThread("Couldn't get Result #2")
+                    }
+                } else {
+                    setTextOnMainThread("Couldn't get Result #1")
+                }
+            }
+            delay(JOB_TIMEOUT)
+            if(job.isActive){
+                job.cancel()
+                job.join()
+                println("debug: Cancelling job...Job took longer than $JOB_TIMEOUT")
+            }
+        }
+    }
+
+    private suspend fun fakeApiJobJoinRequestCaller() {
+        val result = fakeApiJobJoinRequest()
+        // waits until all jobs in coroutine scope are complete to return result
+        println("debug: result: ${result}")
+        setTextOnMainThread(result)
+    }
+
+    private suspend fun fakeApiJobJoinRequest(): String {
+        var timeElapsed = 0L
+        withContext(IO) {
+
+            val time = measureTimeMillis {
+
+                val job1 = launch {
+                    println("debug: starting job 1")
+                    delay(1000)
+                    println("debug: done job 1")
+                }
+
+                val job2 = launch {
+                    println("debug: staring job 2")
+                    delay(1500)
+                    println("debug: done job 2")
+                }
+
+                val job3 = launch {
+                    println("debug: starting job 3")
+                    delay(1000)
+                    println("debug: done job 3")
+                }
+                job1.join()
+                job2.join()
+                job3.join()
+            }
+            timeElapsed = time
+            println("debug: elapsed time: ${timeElapsed}")
+        }
+        return "Jobs completed within $timeElapsed ms."
+    }
+
+    /**
+     * Job pattern
+     * Job1 and Job2 run in parallel as different coroutines
+     * Also see "Deferred, Async, Await" branch(function) for parallel execution
+     */
+    private fun fakeApiJobParallelRequest() {
+        val startTime = System.currentTimeMillis()
+        val parentJob = CoroutineScope(IO).launch {
+            val job1 = launch {
+                val time1 = measureTimeMillis {
+                    println("debug: launching job1 in thread: ${Thread.currentThread().name}")
+                    val result1 = getResult1FromApi()
+                    setTextOnMainThread("Got $result1")
+                }
+                println("debug: compeleted job1 in $time1 ms.")
+            }
+
+            val job2 = launch {
+                val time2 = measureTimeMillis {
+                    println("debug: launching job2 in thread: ${Thread.currentThread().name}")
+                    val result2 = getResult2FromApi()
+                    setTextOnMainThread("Got $result2")
+                }
+                println("debug: compeleted job2 in $time2 ms.")
+            }
+        }
+        parentJob.invokeOnCompletion {
+            println("debug: total elapsed time in ${System.currentTimeMillis() - startTime}")
+        }
+    }
+
+    /**
+     * async() is a blocking call (similar to the job pattern with job.join())
+     * async() must return the type in Deferred<>
+     * async() and await() benefit is can get the result in async { } and is available outside the scoipe
+     * whereas result in job pattern is stuck inside coroutine
+     *  NOTES:
+     *  1) IF you don't call await(), it does not wait for the result
+     *  2) Calling await() on both these Deffered values will EXECUTE THEM IN PARALLEL. But the RESULTS won't
+     *     be published until the last result is available (in this case that's result2)
+     */
+    private fun fakeApiAsyncAwaitDeferredRequest() {
+        CoroutineScope(IO).launch {
+            val executionTime = measureTimeMillis {
+                val result1: Deferred<String> = async {
+                    println("debug: launching job1: ${Thread.currentThread().name}")
+                    getResult1FromApi()
+                }
+
+                val result2: Deferred<String> = async {
+                    println("debug: launching job2: ${Thread.currentThread().name}")
+                    getResult2FromApi()
+                }
+                setTextOnMainThread("Got ${result1.await()}")
+                setTextOnMainThread("Got ${result2.await()}")
+            }
+            println("debug: job1 and job2 are complete. It took ${executionTime} ms")
         }
     }
 
@@ -45,16 +180,14 @@ class MainActivity : AppCompatActivity() {
 
     private suspend fun fakeApiRequestTimeout() {
         withContext(IO) {
-
             val exceptionHandler = CoroutineExceptionHandler{_ , throwable->
                 throwable.printStackTrace()
             }
-
-            val job = withTimeoutOrNull(1900L) {
+            val job = withTimeoutOrNull(JOB_TIMEOUT) {
                 val result1 = getResult1FromApi() // wait until job is done
                 setTextOnMainThread("Got $result1")
 
-                val result2 = getResult2FromApi() // wait until job is done
+                val result2 = getResult2FromApi(result1) // wait until job is done
                 setTextOnMainThread("Got $result2")
 
             } // waiting for job to complete...
@@ -80,7 +213,7 @@ class MainActivity : AppCompatActivity() {
         if (result1 == "Result #1") {
             setTextOnMainThread("Got $result1")
 
-            val result2 = getResult2FromApi() // wait until job is done
+            val result2 = getResult2FromApi(result1) // wait until job is done
 
             if (result2 == "Result #2") {
                 setTextOnMainThread("Got $result2")
@@ -102,7 +235,7 @@ class MainActivity : AppCompatActivity() {
         return "Result #1"
     }
 
-    private suspend fun getResult2FromApi(): String {
+    private suspend fun getResult2FromApi(result1: String = ""): String {
         logThread("getResult2FromApi")
         delay(1000)
         return "Result #2"
